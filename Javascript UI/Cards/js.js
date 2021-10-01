c33b28b4bd85368c0628176eb56e48f0b76e2717
gosmain <gosmain@naver.com>
2021-10-01 16:14:44 +0900

    UI_navigationbar

--- Javascript UI/Cards/js.js
+++ Javascript UI/Cards/js.js
@@ -1,4 +1,4 @@
-var VanillaTilt = (function () {
+let VanillaTilt = (function () {
     'use strict';
     
     class VanillaTilt {
@@ -449,7 +449,7 @@ var VanillaTilt = (function () {
         };
     
         let newSettings = {};
-        for (var property in defaultSettings) {
+        for (let property in defaultSettings) {
           if (property in settings) {
             newSettings[property] = settings[property];
           } else if (this.element.hasAttribute("data-tilt-" + property)) {
