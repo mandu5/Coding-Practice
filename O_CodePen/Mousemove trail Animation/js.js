1959df563bfffba50094bedc25339ab4dc1eadf3
gosmain <gosmain@naver.com>
2021-10-03 22:47:37 +0900

    Codepen_M,P

---
+++ O_CodePen/Mousemove trail Animation/js.js
@@ -0,0 +1,16 @@
+document.addEventListener("mousemove", function(e) {
+    let body = document.querySelector("body");
+    let heart = document.createElement("span");
+    let x = e.offsetX;
+    let y = e.offsetY;
+    heart.style.left = x+"px";
+    heart.style.top = y+"px";
+    let size = Math.random() * 100;
+    heart.style.width = 20 + size + "px";
+    heart.style.height = 20 + size + "px";
+    body.appendChild(heart);
+
+    setTimeout(function(){
+        heart.remove();
+    },1500);
+})
---
+++ O_CodePen/Page Not Found/js.js
@@ -0,0 +1,7 @@
+let container = document.getElementById("container");
+window.onmousemove = function(e) {
+    let x = - e.clientX/5,
+        y = - e.clientY/5;
+        container.style.backgroundPositionX = x + "px";
+        container.style.backgroundPositionY = y + "px";
+}
