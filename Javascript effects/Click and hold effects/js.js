38cb71942a79a1fc56963ba5e5092885ab1c15fa
gosmain <gosmain@naver.com>
2021-10-11 23:13:31 +0900

    Js effects click and hold

---
+++ Javascript effects/Click and hold effects/js.js
@@ -0,0 +1,7 @@
+let cursor = document.getElementById("cursor");
+window.onmousemove = function(e) {
+    let x = e.clientX,
+        y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+}
