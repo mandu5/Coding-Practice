0157cfa6555b64a397efb2653f6132f0b92df4a6
gosmain <gosmain@naver.com>
2021-10-15 01:33:30 +0900

    UI_modal & toggle

---
+++ Javascript UI/Modal UI/Responsive image pop up/js.js
@@ -0,0 +1,4 @@
+let imgBx = document.querySelectorAll(".imgBx");
+imgBx.forEach(popup => popup.addEventListener("click", () => {
+    popup.classList.toggle("active")
+}))
---
+++ Javascript effects/Simple Calender/js.js
@@ -0,0 +1,13 @@
+const lang = navigator.language;
+let date = new Date();
+
+let dayNumber = date.getDate();
+let month = date.getMonth();
+let dayName = date.toLocaleString(lang,{weekday: "long"});
+let monthName = date.toLocaleString(lang,{month: "long"});
+let year = date.getFullYear();
+
+document.getElementById("monthName").innerHTML = monthName;
+document.getElementById("dayName").innerHTML = dayName;
+document.getElementById("dayNumber").innerHTML = dayNumber;
+document.getElementById("year").innerHTML = year;
