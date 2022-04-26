6e589c0edd6253028470c9eb16c692213e69687c
Youngmin <gosmain@naver.com>
2022-04-26 23:07:02 +0900

    react search

---
+++ React_Functions/search_option/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Users } from "./users";
+import "./App.css";
+import Table from "./Table";
+
+// 1) 일반 검색 기능(1개): db(json 파일형식)
+
+// function App() {
+//   const [query, setQuery] = useState("");
+//   return (
+//     <div className="app">
+//       <input
+//         className="search"
+//         placeholder="Search..."
+//         onChange={(e) => setQuery(e.target.value.toLowerCase())}
+//       />
+//       <ul className="list">
+//         {Users.filter((asd) =>
+//           asd.first_name.toLowerCase().includes(query)
+//         ).map((user) => (
+//           <li className="listItem" key={user.id}>
+//             {user.first_name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// 1) 일반 검색 사용방법(여려개): db(json 파일형식)
+
+function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+  return (
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      />
+      {<Table data={search(Users)} />}
+    </div>
+  );
+}
+
+export default App;
